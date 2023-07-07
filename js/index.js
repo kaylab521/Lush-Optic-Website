4f2459a2a9bdfbe66060718a27194913ee901236
kaylab521 <99372735+kaylab521@users.noreply.github.com>
2023-07-07 02:36:23 -0700

    Uploaded Files

---
+++ js/index.js
@@ -0,0 +1,10 @@
+function openForm(commissions) {
+  document.getElementById("commissions").style.display = "block";
+}
+
+function openForm(bookings) {
+  document.getElementById("bookings").style.display = "block";
+}
+
+
+ 
