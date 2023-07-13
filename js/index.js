712aac5bfed63e4085d988a11c14a82a2a009b48
kaylab521 <Michaela.b521@gmail.com>
2023-07-12 20:55:48 -0600

    update index.js

--- js/index.js
+++ js/index.js
@@ -6,7 +6,13 @@ function openForm(bookings) {
   document.getElementById("bookings").style.display = "block";
 }
 
+function closeForm(commission) {
+  document.getElementById("bookings").style.display = "none";
+}
 
+function closeForm(bookings) {
+  document.getElementById("commission").style.display = "none";
+}
 /*function click(dropdown) {
   document.getElementById("dropdown").style.display = "block";
 }*/
